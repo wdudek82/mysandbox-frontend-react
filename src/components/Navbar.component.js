4a887b4f464266a2f42aa8369ce94b6f57f5f066
Wojciech Dudek <wdudek82@gmail.com>
2017-03-09 23:03:38 +0100

    in JSX tags class -> className

--- src/components/Navbar.component.js
+++ src/components/Navbar.component.js
@@ -33,13 +33,13 @@ class Navbar extends React.Component {
 
                <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                   <ul className="nav navbar-nav">
-                     <li class={featuredClass}>
+                     <li className={featuredClass}>
                         <IndexLink to="/" >Featured</IndexLink>
                      </li>
-                     <li class={archivesClass}>
+                     <li className={archivesClass}>
                         <Link to="archives">Archives</Link>
                      </li>
-                     <li class={settingsClass}>
+                     <li className={settingsClass}>
                         <Link to="settings">Settings</Link>
                      </li>
                   </ul>
